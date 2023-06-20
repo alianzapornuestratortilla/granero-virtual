81c0608bf062bc690e3a2473e15b3855dba0dce6
dpicassom <dpicasso000@gmail.com>
2023-06-20 15:03:08 -0600

    ya se muestran y oucultan las que son

---
+++ js/despliegaOpciones.js
@@ -0,0 +1,114 @@
+
+function checkboxTranslator(daValue) {
+    let lado;
+    switch (daValue) {
+        case true:
+            lado = "derecha";
+            break;
+        case false:
+            lado = "izquierda";
+            break
+
+        default:
+            console.error('checkbox de shrodinger');
+            lado = false;
+            break;
+
+    }
+    return lado;
+}
+
+function ladoOpuesto(elLado) {
+    let lado;
+    switch (elLado) {
+        case "izquierda":
+            lado = "derecha";
+            break;
+        case "derecha":
+            lado = "izquierda";
+            break
+
+        default:
+            console.error('lado de shrodinger');
+            lado = false;
+            break;
+
+    }
+    return lado;
+}
+
+
+function despliegaOcultaOpciones(childrenOfParentComponent, show, inventarioDeSwitches) {
+    
+    let displayProperty
+    for (const currentComponentKey in childrenOfParentComponent) {
+        switch (show) {
+            case "show":
+                // mostrarlo
+                displayProperty = "";
+                break;
+            // si es switch y en mostrar, evalua valor, si no                
+            case "hide":
+                // solo ocultalo
+                displayProperty = "none";
+
+                break;
+
+            default:
+
+                break;
+        }
+
+        const currentComponent = childrenOfParentComponent[currentComponentKey];
+        document.getElementById(currentComponentKey).style.display = displayProperty;
+        // debugger
+        if ((currentComponent.hasOwnProperty("izquierda") && currentComponent.hasOwnProperty("derecha"))) {
+            //es switch
+            let components2beDisplayed = new Array();
+            let components2beHidden = new Array();
+            switch (show) {
+                case "show":
+                    //chequea status del switch
+                    const currentSwitchStatement = currentComponentKey;
+                    const currentLado = inventarioDeSwitches[currentSwitchStatement];
+                    const currentSwitch = childrenOfParentComponent[currentSwitchStatement];
+                    components2beDisplayed.push(currentSwitch[currentLado]);
+                    components2beHidden.push(currentSwitch[ladoOpuesto(currentLado)]);
+                    break;
+                case "hide":
+                    // oculta sus descendientes de de ambos lados
+                    components2beHidden.push(currentComponent["izquierda"]);
+                    components2beHidden.push(currentComponent["derecha"]);
+                    break;
+
+                default:
+                    break;
+            }           
+
+            //ocultar lado opuesto y todos los descendientes
+            components2beHidden.forEach(component => {
+                void despliegaOcultaOpciones(component, "hide", inventarioDeSwitches);
+            });
+            
+            // mostrar lado seleccionado evaluando
+            components2beDisplayed.forEach(component => {
+                void despliegaOcultaOpciones(component, "show", inventarioDeSwitches);
+            });
+
+        }
+        // si no es switch ya estuvo bueno
+
+    }
+
+
+}
+
+function despliegalasOpciones(diccionarioDeOpciones) {
+    const daSwitches = document.querySelectorAll('label.switch input[type="checkbox"]');
+    let daSwitchInventory = new Object();
+    for (let i = 0; i < daSwitches.length; i++) {
+        let daSwitch = daSwitches[i];
+        daSwitchInventory[daSwitch.closest('div.switch-statement').id] = checkboxTranslator(daSwitch.checked);
+    }
+    void despliegaOcultaOpciones(diccionarioDeOpciones, "show", daSwitchInventory);
+}
--- js/registroSwitches.js
+++
@@ -1,8 +0,0 @@
-function despliegaOpcionesCorrectas() {
-  const daSwitches = document.querySelectorAll('label.switch input[type="checkbox"]');
-  let daSwitchInventory = new Object();
-  for (let i = 0; i < daSwitches.length; i++) {
-    let daSwitch = daSwitches[i];
-    daSwitchInventory[daSwitch.closest('div.switch-statement').id] = !daSwitch.checked;
-  }
-}
