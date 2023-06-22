ac57093357af3726aa852cdaff7a70f4f715339d
dpicassom <dpicasso000@gmail.com>
2023-06-22 09:08:21 -0600

    ya funciona el checkbox

--- js/checkDaBox.js
+++ js/checkDaBox.js
@@ -1,4 +1,13 @@
 function checkDaBox(event) {
+    console.log('entró');
     let daCheckbox = event.target.closest('div.check-box').querySelector('input[type="checkbox"]');
+    // debugger
+    console.log(daCheckbox.checked);
     daCheckbox.checked = !daCheckbox.checked;
+    console.log(daCheckbox.checked);
+    console.log('sale');
+    // debugger
+    // 
+    // daCheckbox.click();
+    // debugger
 }
