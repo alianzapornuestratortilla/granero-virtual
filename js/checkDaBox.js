090e3db1986e213d72ab9f5df934562d2b8d8e58
dpicassom <dpicasso000@gmail.com>
2023-06-22 14:26:31 -0600

    se vuelven y dejan de ser required los input ocultos

--- js/checkDaBox.js
+++ js/checkDaBox.js
@@ -2,10 +2,11 @@ function checkDaBox(event) {
     console.log('entró');
     let daCheckbox = event.target.closest('div.check-box').querySelector('input[type="checkbox"]');
     // debugger
-    console.log(daCheckbox.checked);
+    // console.log(daCheckbox.checked);
     daCheckbox.checked = !daCheckbox.checked;
-    console.log(daCheckbox.checked);
-    console.log('sale');
+    daCheckbox.dispatchEvent(new Event('change'));
+    // console.log(daCheckbox.checked);
+    // console.log('sale');
     // debugger
     // 
     // daCheckbox.click();
---
+++ js/otros.js
@@ -0,0 +1,48 @@
+function añadeOtro(listaID, templateID) {
+    document.getElementById(listaID).appendChild(document.getElementById(templateID).content.cloneNode(true));
+}
+function killOtro(event) {
+    const listaDeOtrosContainer = event.target.closest('fieldset.lista-de-otros-container');
+    event.target.closest('fieldset.otro').remove();
+    if (listaDeOtrosContainer.querySelector('div.lista-de-otros').querySelectorAll('fieldset.otro').length == 0) {
+        listaDeOtrosContainer.querySelector('button.btn-añade').dispatchEvent(new Event('click'));
+    }
+}
+
+function showHideOtros(event) {
+    const elements = event.target.getAttribute('otros').split(' ');
+    const container = event.target.closest('fieldset.lista-de-otros-container');
+    const isChecked = event.target.checked;
+    let display
+    switch (isChecked) {
+        case true:
+            display = "block";
+            break;
+        case false:
+            display = "none";
+            break;
+
+        default:
+            break;
+    }
+
+    elements.forEach(element => {
+        element = container.querySelector(element);
+        element.style.display = display;
+        // debugger
+        if (element.tagName == "DIV" && element.classList.contains('lista-de-otros')) {
+
+            const required = isChecked;
+            // debugger
+            const fieldsetsDeOtro = element.childNodes;
+
+            for (let index = 0; index < fieldsetsDeOtro.length; index++) {
+                const fieldsetDeOtro = fieldsetsDeOtro[index];
+                if (fieldsetDeOtro.tagName == "FIELDSET") {                    
+                    fieldsetDeOtro.querySelector('input.input-otros').required = required;
+                }
+
+            }
+        }
+    });
+}
